c463579daa5d18823c9d4d93391f6711ed631a48
jardzrey <jardzrey@gmail.com>
2019-03-24 02:21:13 -0600

    primer commit

---
+++ precache-manifest.d71eae7b1a643fbed263bbf67def0e83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4112b1147691646b8e3",
+    "url": "/static/css/main.dbd1f940.chunk.css"
+  },
+  {
+    "revision": "b4112b1147691646b8e3",
+    "url": "/static/js/main.52d2deaa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "b4620d4d06731c60cf766880ae51b1c1",
+    "url": "/index.html"
+  }
+];
